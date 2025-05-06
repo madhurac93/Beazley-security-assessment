be3b1a682d83032e470b715b67e360539766d9d2
madhurac93 <128417602+madhurac93@users.noreply.github.com>
2025-05-05 18:39:01 -0700

    Update outputs.tf

--- Beazley-assessment/Task - 1/outputs.tf
+++ Beazley-assessment/Task - 1/outputs.tf
@@ -8,13 +8,9 @@ output "vnet_id" {
   description = "ID of the Virtual Network"
 }
 
-output "web_subnet_id" {
-  value       = module.network.web_subnet_id
-  description = "ID of the Web subnet"
-}
 
-output "app_subnet_id" {
-  value       = module.network.app_subnet_id
+output "aks_subnet_id" {
+  value       = module.network.aks_subnet_id
   description = "ID of the Application subnet"
 }
 
@@ -23,22 +19,13 @@ output "db_subnet_id" {
   description = "ID of the Database subnet"
 }
 
-output "web_tier_apps" {
-  value       = module.web_tier.web_apps
-  description = "List of Web Tier App Service names"
-}
-
-output "app_tier_apps" {
-  value       = module.app_tier.app_services
-  description = "List of Application Tier App Service names"
-}
 
 output "db_tier_fqdn" {
-  value       = module.db_tier.db_server_fqdn
+  value       = module.db.db_server_fqdn
   description = "Fully qualified domain name of the database server"
 }
 
 output "db_private_endpoint_ip" {
-  value       = module.db_tier.private_endpoint_ip
+  value       = module.db.private_endpoint_ip
   description = "Private IP of the database private endpoint"
 }
